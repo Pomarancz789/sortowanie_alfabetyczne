a7d321524691b0c7752399de5eae547b84d5d58f
Pomarancz789 <granat-02krawcy@icloud.com>
2022-05-25 10:34:54 +0200

    Initial Commit

---
+++ sortowanie_alfabetyczne/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sortowanie_alfabetyczne
+//
+//  Created by Jakub Wtorkiewicz on 25/05/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
